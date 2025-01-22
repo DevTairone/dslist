1266ac314bb60cb416c6db8b2b7dcaa21881390a
DevTairone <167230880+DevTairone@users.noreply.github.com>
2025-01-22 12:30:32 -0300

    complete GET lists

--- src/main/java/com/devtairone/dslist/services/GameService.java
+++ src/main/java/com/devtairone/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devtairone.dslist.services;
 import com.devtairone.dslist.DTO.GameDTO;
 import com.devtairone.dslist.DTO.GameMinDTO;
 import com.devtairone.dslist.entities.Game;
+import com.devtairone.dslist.projections.GameMinProjection;
 import com.devtairone.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
